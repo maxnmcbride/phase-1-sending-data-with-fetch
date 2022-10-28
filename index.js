bb5149fdec15bd0d2423e7e61d030b7d76804745
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2022-10-28 10:58:33 -0400

    commit tree ed44c1b24ac5b7deb6a16e2017ca217a82cc0d62

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+const docBody = document.querySelector('body');
+
+function submitData(){
+    return fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers:{
+            'Content-Type' : 'application/json',
+            'Accept' : 'application/json',
+        },
+        body: JSON.stringify({
+            name: 'Steve',
+            email: 'steve@steve.com'
+        }),
+    })
+        .then(response => response.json())
+        .then((users) =>{
+            docBody.innerText = users.id;
+        })
+        .catch(function(error){
+            alert('Something went wrong!');
+            docBody.textContent = error.message;
+        });
+}
+
+submitData();
